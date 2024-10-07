7dd75afb5e50e841cb31d3e0fa0e19447822268e
Li Haoyuan <1513624626@qq.com>
2024-10-07 21:45:55 +0800

    feat: add launch main function

--- zsh_launch_master.zsh
+++ zsh_launch_master.zsh
@@ -1,31 +1,53 @@
 #!/bin/zsh
 
-alias launch_1='launch_master_launch'
-alias launch_master='launch_master_opts'
+alias launch_1='launch_master_main'
+
+declare launch_master_lnks=''
+declare launch_master_launch_list=''
+declare launch_master_open_cmd=''
 
 # init steps
-if [[ -f ~/.launch_master ]]; then # check if launch_master is already initialized
+if [[ -f $HOME/.launch_master/.launch_masterrc ]]; then # check if launch_master is already initialized
+  while LFS= read -r line; do
+		if [[ $line == \#* || $line == +([[:space:]]) ]]; then # skip comment line
+			continue
+		fi
+    case ${line%:*} in
+    	'launch_master_lnks')
+				launch_master_lnks=${line##*:}
+				continue
+			;;
+			'launch_master_launch_list')
+				launch_master_launch_list=${line##*:}
+				continue
+			;;
+			'launch_master_open_cmd')
+				launch_master_open_cmd=${line##*:}
+				continue
+			;;
+			*)
+				echo "Unknown item \""${line%:*}"\" in .launch_masterrc"
+				continue
+			;;
+		esac
+	done < $HOME"/.launch_master/.launch_masterrc"
+else # first open, process the initialize steps
+	source $(dirname $(readlink -f $0))/launch_master_init.zsh
+fi
 
-  
-
-
-
-declare launch_master_open_cmd=''
-case $OSTYPE in
-  'msys')
-    launch_master_open_cmd='start'
-  ;;
-  'linux')
-    launch_master_open_cmd='xdg-open' # depending on xdg-utils
-  ;;
-  'darwin')
-    launch_master_open_cmd='open'
-  ;;
-  *)
-    echo "Launch Master:\n\tWarning: Unsupport OS \""$OSTYPE"\"\n\tLaunch Master is unable to launch applications"
-    return 1
-  ;;
-esac
+function launch_master_main() {
+  while (($# > 0)); do
+    case '$1' in
+      '-o'|'--opts')
+        shift
+	launch_master_opts $@
+      ;;
+      *)
+	launch_master_launch $@
+      ;;
+    esac
+  done
+}
 
 function launch_master_opts() {
   while (($# > 0)); do
@@ -58,18 +80,22 @@ function launch_master_opts() {
   done
 }
 
-function launch_master_launch() { # launch applications
+function launch_master_launch() {
   while (($# > 0)); do
-    local app_lnk="./links/"$1".lnk"
-    if [[ -f $app_lnk ]]; then
-      echo "Launching "$1".lnk..."
-      nohup $launch_master_open_cmd $app_lnk &>/dev/null
+    if [ ${1##*.} = "launch_list" ]; then
+      while LFS= read -r line; do
+        launch_master_launch $line
+      done < "./launch_lists/"$1
+      shift
     else
-      echo "No such application: "$1".lnk"
+      local app_lnk="./links/"$1".lnk"
+      if [[ -f $app_lnk ]]; then
+        echo "Launching "$1".lnk..."
+        nohup $launch_master_open_cmd $app_lnk &>/dev/null
+      else
+        echo "No such application: "$1".lnk"
+      fi
+      shift
     fi
-    shift
   done
 }
-
-
-
